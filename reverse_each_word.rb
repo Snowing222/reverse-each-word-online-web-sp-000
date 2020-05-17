76eda2a2b8823cf51fffdfbe90dddf503646ecd4
elsa chen <snowing222@gmail.com>
2020-05-17 02:44:25 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,14 +1,5 @@
-=begindef reverse_each_word(string)
-  array=string.split(" ")
-  reverse_array=[]
-  array.each do |x|
-    reverse_array.push(x.reverse)
-  end
 
-  reverse_array.join(" ")
 
-end
-=end
 def reverse_each_word(string)
   array=string.split(" ")
   array.collect {|x| x.reverse}
